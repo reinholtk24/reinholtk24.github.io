90cce8d67b8eca4f02eda119e261e407c39501e2
Kyle <reinholtk24@mycu.concord.edu>
2018-04-19 14:01:39 -0600

    Adding local css/js

--- test_excel_browser.php
+++ test_excel_browser.php
@@ -1,21 +1,28 @@
 <?php
 
-require "php-export-data.class.php";
+// When executed in a browser, this script will prompt for download 
+// of 'test.xls' which can then be opened by Excel or OpenOffice.
 
-$excel = new ExportDataExcel('browser');
-$excel->filename = "test.xml";
+require 'php-export-data.class.php';
 
-$data = array(
-	array(1,2,3),
-	array("asdf","jkl","semi"), 
-	array("1273623874628374634876","=asdf","10-10"),
-	array("2010-01-02 10:00AM","1/1/11","10-10"),
-	array("1234","12.34","-123."),
-	array("-12345678901234567890","0.0000000000123456789","-"),
-);
+// 'browser' tells the library to stream the data directly to the browser.
+// other options are 'file' or 'string'
+// 'test.xls' is the filename that the browser will use when attempting to 
+// save the download
+$exporter = new ExportDataExcel('browser', 'test.xls');
 
-$excel->initialize();
-foreach($data as $row) {
-	$excel->addRow($row);
-}
-$excel->finalize();
+$exporter->initialize(); // starts streaming data to web browser
+
+// pass addRow() an array and it converts it to Excel XML format and sends 
+// it to the browser
+$exporter->addRow(array("This", "is", "a", "test")); 
+$exporter->addRow(array(1, 2, 3, "123-456-7890"));
+
+// doesn't care how many columns you give it
+$exporter->addRow(array("foo")); 
+
+$exporter->finalize(); // writes the footer, flushes remaining data to browser.
+
+exit(); // all done
+
+?>
