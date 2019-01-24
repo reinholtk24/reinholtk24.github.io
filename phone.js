d6b422e7a93dbf1dde1140b54207de8714990551
reinholtk24 <kyle.reinholt-1@colorado.edu>
2019-01-23 17:45:23 -0700

    changing for assignment

--- phone.js
+++ phone.js
@@ -3,7 +3,6 @@ $(document).ready(function() { // do this when the document is loaded
 	$("#content_dialer").show(); // show the element with ID "element"
 	$("#content_list").hide(); // hide the element with ID "otherElement"
 	$("#content_add").hide();
-	alert("hi");
 });
 
 $("#button1").click(function() { // when "button_id" is clicked
@@ -24,4 +23,75 @@ $("#button3").click(function() { // when "button_id" is clicked
 	$("#content_list").hide();
 });
 
+$("#button3").click(function() { // when "button_id" is clicked
+	$("#content_add").show(); // show element
+	$("#content_dialer").hide(); // hide the element with ID "otherElement"
+	$("#content_list").hide();
+});
+
+$("#1").click(function() { // when "button_id" is clicked
+	var $nums = $('#inputDialer');
+	$nums.val($nums.val() + "1"); 
+	
+});
+
+$("#2").click(function() { // when "button_id" is clicked
+	var $nums = $('#inputDialer');
+	$nums.val($nums.val() + "2"); 
+	
+});
+
+$("#3").click(function() { // when "button_id" is clicked
+	var $nums = $('#inputDialer');
+	$nums.val($nums.val() + "3"); 
+	
+});
+
+$("#4").click(function() { // when "button_id" is clicked
+	var $nums = $('#inputDialer');
+	$nums.val($nums.val() + "4"); 
+	
+});
+
+$("#5").click(function() { // when "button_id" is clicked
+	var $nums = $('#inputDialer');
+	$nums.val($nums.val() + "5"); 
+	
+});
+
+$("#6").click(function() { // when "button_id" is clicked
+	var $nums = $('#inputDialer');
+	$nums.val($nums.val() + "6"); 
+	
+});
+
+$("#7").click(function() { // when "button_id" is clicked
+	var $nums = $('#inputDialer');
+	$nums.val($nums.val() + "7"); 
+	
+});
+
+$("#8").click(function() { // when "button_id" is clicked
+	var $nums = $('#inputDialer');
+	$nums.val($nums.val() + "8"); 
+	
+});
+
+$("#9").click(function() { // when "button_id" is clicked
+	var $nums = $('#inputDialer');
+	$nums.val($nums.val() + "9"); 
+	
+});
+
+$("#pound").click(function() { // when "button_id" is clicked
+	var $nums = $('#inputDialer');
+	$nums.val($nums.val() + "#"); 
+	
+});
+
+$("#star").click(function() { // when "button_id" is clicked
+	var $nums = $('#inputDialer');
+	$nums.val($nums.val() + "*"); 
+	
+});
 
