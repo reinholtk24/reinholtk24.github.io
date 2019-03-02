1dc9c628dc0a002326f455e5034a40c915572a6c
reinholtk24 <kyle.reinholt-1@colorado.edu>
2019-03-01 23:14:57 -0700

    v4

--- ApexLegendsBoys/script.js
+++ ApexLegendsBoys/script.js
@@ -1,6 +1,7 @@
 
 var locations = ["Slum Lakes","Artillery","Relay","Wetlands","Cascades","The Pit","Runoff","Bunker","Swamps","Airbase","Bridges","Hydro Dam","Repulsor","Market","Skull Town","Thunderdome","Water Treatment","Hot Zone Boys","Ship Bitches","Jump Master Picks Boys"]
 var characters = ["Bloodhound","Gibraltar","Lifeline","Pathfinder","Wraith","Bangalore","Caustic","Mirage"]
+var insults = ["Idiot", "Turkey boy", "Poop head", "Baby Head", "Dummy baby boy", "Pizza boy", "little biscuit bitch"]
 
 
 window.addEventListener("load", function(e) {
@@ -14,5 +15,11 @@ window.addEventListener("load", function(e) {
 		 var rand = Math.floor(Math.random() * characters.length);
 		 document.getElementById("characters").innerHTML = "Character: " + characters[rand];
 	})
+	
+	document.querySelector('#codyButton').addEventListener('mousedown',() => {
+		 var rand = Math.floor(Math.random() * insults.length);
+		 document.getElementById("cody").innerHTML = "Insult: " + insults[rand];
+	})
+	
 	
 });
