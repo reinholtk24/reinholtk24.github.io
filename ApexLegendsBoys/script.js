400d4be5efd3122ed01f096e788ee452ea0a2c66
reinholtk24 <kyle.reinholt-1@colorado.edu>
2019-03-01 23:00:27 -0700

    v3

--- ApexLegendsBoys/script.js
+++ ApexLegendsBoys/script.js
@@ -1,11 +1,18 @@
 
 var locations = ["Slum Lakes","Artillery","Relay","Wetlands","Cascades","The Pit","Runoff","Bunker","Swamps","Airbase","Bridges","Hydro Dam","Repulsor","Market","Skull Town","Thunderdome","Water Treatment","Hot Zone Boys","Ship Bitches","Jump Master Picks Boys"]
+var characters = ["Bloodhound","Gibraltar","Lifeline","Pathfinder","Wraith","Bangalore","Caustic","Mirage"]
+
 
 window.addEventListener("load", function(e) {
 	
 	document.querySelector('#test').addEventListener('mousedown',() => {
 		 var rand = Math.floor(Math.random() * locations.length);
-		 document.getElementById("status").innerHTML = locations[rand];
+		 document.getElementById("status").innerHTML = "Location: " + locations[rand];
+	})
+	
+	document.querySelector('#char').addEventListener('mousedown',() => {
+		 var rand = Math.floor(Math.random() * characters.length);
+		 document.getElementById("characters").innerHTML = "Character: " + characters[rand];
 	})
 	
 });
