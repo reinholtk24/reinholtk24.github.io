5863a8eac589939feb83b7b3079adb2a06700c3e
reinholtk24 <kyle.reinholt-1@colorado.edu>
2019-07-03 16:01:44 -0600

    Season 2

--- ApexLegendsBoys/script.js
+++ ApexLegendsBoys/script.js
@@ -1,7 +1,8 @@
 
 var locations = ["Slum Lakes","Artillery","Relay","Wetlands","Cascades","The Pit","Runoff","Bunker","Swamps","Airbase","Bridges","Hydro Dam","Repulsor","Market","Skull Town","Thunderdome","Water Treatment","Hot Zone Boys","Ship Bitches","Jump Master Picks Boys"]
-var characters = ["Bloodhound","Gibraltar","Lifeline","Pathfinder","Wraith","Bangalore","Caustic","Mirage"]
-var insults = ["Idiot", "Turkey boy", "Poop head", "Baby Head", "Dummy baby boy", "Pizza boy", "little biscuit bitch"]
+var characters = ["Bloodhound","Gibraltar","Lifeline","Pathfinder","Wraith","Bangalore","Caustic","Mirage", "Octane", "Wattson"]
+var insults = ["Baby Head", "Dummy baby boy", "Pizza boy", "little biscuit bitch" , "Dino head", "Sand boy"]
+var guns = ["VK-47","Flatline","Alternator","Mozambique","G7 Scout","P2020","Havoc","Prowler","Peacekeeper","Longbow DMR","RE-45 Auto","Hemlok","R-99","EVA-8 Auto","Triple Take","Wingman","R-301 Carbine","M600 Spitfire","Mastiff","Kraber .50-CAL","Devotion","L-Star EMG"]
 
 
 window.addEventListener("load", function(e) {
@@ -14,12 +15,25 @@ window.addEventListener("load", function(e) {
 	document.querySelector('#char').addEventListener('mousedown',() => {
 		 var rand = Math.floor(Math.random() * characters.length);
 		 document.getElementById("characters").src= "Characters/" + characters[rand] + ".jpg";
+		 document.getElementById("characters").width = 200;
+		 document.getElementById("characters").height = 250; 
 		 //document.getElementById("characters").innerHTML = "Character: " + characters[rand];
 	})
 	
 	document.querySelector('#codyButton').addEventListener('mousedown',() => {
 		 var rand = Math.floor(Math.random() * insults.length);
 		 document.getElementById("cody").innerHTML = "Insult: " + insults[rand];
+	})
+	
+	document.querySelector('#win').addEventListener('mousedown',() => {
+		 var rand = Math.floor(Math.random() * 100 );
+		 var rand2 = Math.floor(Math.random() * 100); 
+		 document.getElementById("chance").innerHTML = "Win Chance: " + rand + "." + rand2 + "%";
+	})
+	
+	document.querySelector('#gun').addEventListener('mousedown',() => {
+		 var rand = Math.floor(Math.random() * guns.length);
+		 document.getElementById("guns").innerHTML = "Main Weapon: " + guns[rand];
 	})
 	
 	
